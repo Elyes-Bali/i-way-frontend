a13dbbc7af8afb9f02b4628f119f11647080dc4f
Elyes Bali <baliaynos@gmail.com>
2025-07-26 20:51:07 +0100

    ws

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: false,
   //apiBaseUrl: 'http://api_service:8080',
-  apiBaseUrl: 'http://192.168.50.4:8090',
+  apiBaseUrl: 'http://192.168.50.4:8090/api',
 // apiBaseUrl: 'http://localhost:8080',
  apiAiUrl: 'http://192.168.50.4:5000'
  // apiAiUrl: 'http://localhost:5000/'
